92896975ea80c208630891154753f7bb6e80fb81
Adrian McMenamin <adrianmcmenamin@gmail.com>
2010-08-14 00:32:24 +0100

    working on RT algoritithm - first (crude?) attempt

--- node.cpp
+++ node.cpp
@@ -57,22 +57,54 @@ Tree::~Tree()
 	}
 }
 
-void Tree::calcpoints(Node* n, int level, Node* leftmost, Node* rightmost)
+void Tree::calcpoints(Node* n, int level)
 {
 	n->yco = level;
 	if (n->left != -1)
-		calcpoints(items[n->left], level + 1, n, rightmost);
+		calcpoints(items[n->left], level + 1 );
 	if (n->right != -1)
-		calcpoints(items[n->right], level + 1, leftmost, n);
+		calcpoints(items[n->right], level + 1);
 
-	if (!n->left && !n->right) {
-	}}	
+	Node* left = NULL;
+	Node* right = NULL;
+	int ileft = n->left;
+	if (ileft != 1)
+		left = items[ileft];
+	
+	int iright = n->right;
+	if (iright != -1)
+		right = items[iright];
 
+	while (left && right) {
+		if (left->right != -1 && right->left != -1) {
+			n->offset = n->offset + distance;
+			left = items[left->right];
+			right = items[right->left];
+		}
+		else if (left->right != -1) {
+			left = items[left->right];
+			if (right->right != -1) 
+				right = items[right->right];
+			else
+				break;
+		}
+		else {
+			if (left->left != -1)
+				left = items[left->left];
+			else
+				break;
+			if (right->left != -1)
+				right = items[right->left];
+			else
+				break;
+		}
+	}
+}
 
 void Tree::position()
 {
 	if (items.size() == 0)
 		return;
 	//post order traversal
-	calcpoints(items[0], 0, items[0], items[0]);
+	calcpoints(items[0], 0);
 }
